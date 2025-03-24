58460f819edd4e20ebe90dac81f55b61b5d04f56
Jared Jin <jared.jj.jin@gmail.com>
2025-03-25 09:42:26 +1100

    changed enA and enB pins to match pwm pins on arduino.
    used copilot to create drive function which takes direction and speed.

--- main.ino
+++ main.ino
@@ -1,11 +1,11 @@
 // Arduino Line Follower Robot Code
 //More Information: https://circuitdigest.com/microcontroller-projects/arduino-uno-line-follower-robot 
-#define enA 7//Enable1 L293 Pin enA 
+#define enA 11//Enable1 L293 Pin enA 
 #define in1 6 //Motor1  L293 Pin in1 
 #define in2 5 //Motor1  L293 Pin in1 
 #define in3 9 //Motor2  L293 Pin in1 
 #define in4 10 //Motor2  L293 Pin in1 
-#define enB 8 //Enable2 L293 Pin enB 
+#define enB 3 //Enable2 L293 Pin enB 
 #define R_S 4//ir sensor Right
 #define L_S 2 //ir sensor Left
 #define M_s 12//ir sensor Middle
@@ -20,40 +20,52 @@ pinMode(in3, OUTPUT);
 pinMode(in4, OUTPUT); 
 pinMode(enB, OUTPUT);
 digitalWrite(enA, HIGH); 
-digitalWrite(enB, HIGH); 
-delay(1000);
+digitalWrite(enB, HIGH);
+
+
 float error = 0.0;
 float kp = 1;
 float ki = 0;
 float kd = 0;
+
+
+delay(1000);
 }
 void loop(){  
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 0)){forward();}   //if Right Sensor and Left Sensor are at White color then it will call forword function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 0)){turnRight();} //if Right Sensor is Black and Left Sensor is White then it will call turn Right function  
-if((digitalRead(R_S) == 0)&&(digitalRead(L_S) == 1)){turnLeft();}  //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
-if((digitalRead(R_S) == 1)&&(digitalRead(L_S) == 1)){forward();} //if Right Sensor and Left Sensor are at Black color then it will call Stop function
+
 }
-void forward(){  //forword
-digitalWrite(in1, HIGH); //Right Motor forword Pin 
-digitalWrite(in2, LOW);  //Right Motor backword Pin 
-digitalWrite(in3, LOW);  //Left Motor backword Pin 
-digitalWrite(in4, HIGH); //Left Motor forword Pin 
-}
-void turnRight(){ //turnRight
-digitalWrite(in1, LOW);  //Right Motor forword Pin 
-digitalWrite(in2, HIGH); //Right Motor backword Pin  
-digitalWrite(in3, LOW);  //Left Motor backword Pin 
- digitalWrite(in4, HIGH); //Left Motor forword Pin 
-}
-void turnLeft(){ //turnLeft
-digitalWrite(in1, HIGH); //Right Motor forword Pin 
-digitalWrite(in2, LOW);  //Right Motor backword Pin 
-digitalWrite(in3, HIGH); //Left Motor backword Pin 
-digitalWrite(in4, LOW);  //Left Motor forword Pin 
-}
-void Stop(){ //stop
-digitalWrite(in1, LOW); //Right Motor forword Pin 
-digitalWrite(in2, LOW); //Right Motor backword Pin 
-digitalWrite(in3, LOW); //Left Motor backword Pin 
-digitalWrite(in4, LOW); //Left Motor forword Pin 
+
+void drive(float direction, float speed) {
+    // Clamp direction to the range [-1, 1]
+    if (direction > 1) direction = 1;
+    if (direction < -1) direction = -1;
+
+    // Clamp speed to the range [0, 1]
+    if (speed > 1) speed = 1;
+    if (speed < 0) speed = 0;
+
+    // Calculate motor speeds based on direction and speed
+    float rightMotorSpeed = speed * (1 - direction);
+    float leftMotorSpeed = speed * (1 + direction);
+
+    // Set motor speeds
+    analogWrite(enA, (int)(rightMotorSpeed * 255)); // Right motor speed
+    analogWrite(enB, (int)(leftMotorSpeed * 255));  // Left motor speed
+
+    // Set motor directions
+    if (rightMotorSpeed > 0) {
+        digitalWrite(in1, HIGH);
+        digitalWrite(in2, LOW);
+    } else {
+        digitalWrite(in1, LOW);
+        digitalWrite(in2, LOW);
+    }
+
+    if (leftMotorSpeed > 0) {
+        digitalWrite(in4, HIGH);
+        digitalWrite(in3, LOW);
+    } else {
+        digitalWrite(in4, LOW);
+        digitalWrite(in3, LOW);
+    }
 }
